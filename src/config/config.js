9ee462d93b896afad9cec88924c4982c03053b8b
atishay11jain <88791784+atishay11jain@users.noreply.github.com>
2025-07-17 05:30:01 +0000

    database url configured

---
+++ src/config/config.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const development = {
+  username: "root",
+  password: "AtishayCar@0987",
+  database: "Authentication",
+  host: "127.0.0.1",
+  dialect: "mysql",
+};
+
+const production = {
+  use_env_variable: process.env.DATABASE_URL,
+  dialect: "mysql",
+};
+
+module.exports = {
+  development,
+  production,
+};
--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
-const SALT_ROUND = process.env.SALT_ROUND || 10;
+const SALT_ROUND = parseInt(process.env.SALT_ROUND) || 10;
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "authentication_key";
 const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 const DB_SYNC = process.env.DB_SYNC === 'true';
